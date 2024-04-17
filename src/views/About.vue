ca16367f93cd0783cf262cde68a86da2632fe8de
仔仔 <1375560445@qq.com>
2024-04-17 19:41:01 +0800

    vue3 基础框架示例

---
+++ src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <span>{{ double }}</span>  <br>
+    <span>{{ count }}</span>
+    <p>
+      <button @click="increment">增加</button>
+    </p>
+  </div>
+</template>
+<script>
+import {computed} from 'vue'
+import { useCounterStore } from '../store/index'
+export default {
+  setup() {
+    const users = useCounterStore()
+    const handleClick = ()=>{
+      users.increment()
+    }
+    return{
+      double:computed(()=> users.double),
+      count:computed(()=> users.count),
+      increment:handleClick
+    }
+  },
+}
+</script>
